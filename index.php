09a7b458cbc8a46364472a9f3e6dbb70b30926be
Pratixa Patel <parikshit8410@gmail.com>
2020-03-19 19:09:27 -0400

    These files are adding for very first time

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include 'view/header.php'; ?>
+<main>
+    <nav>
+        
+    <h2>Administrators</h2>
+    <ul>
+        <li><a href="product_manager/show_products.php">Manage Products</a></li>
+        <li><a href="Tech/tech_list.php">Manage Technicians</a></li>
+        <li><a href="Manage_customer/Select_customer.php">Manage Customers</a></li>
+        <li><a href="create_incident/get_customer.php">Create Incident</a></li>
+        <li><a href="under_construction.php">Assign Incident</a></li>
+        <li><a href="under_construction.php">Display Incidents</a></li>
+    </ul>
+
+    <h2>Technicians</h2>    
+    
+
+    <h2>Customers</h2>
+    <ul>
+        <li><a href="rProduct/Cust_login.php">Register Product</a></li>
+    </ul>
+    
+    </nav>
+</section>
+<?php include 'view/footer.php'; ?>
